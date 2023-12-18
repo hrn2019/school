8a5d5048af16b757c4744413c12e1f6d38531c80
ErdeMkl <kerdem813@gmail.com>
2023-12-18 16:06:23 +0300

    StudentClassAdded

---
+++ src/Student.java
@@ -0,0 +1,21 @@
+//Student neye sahiptir? = kalem,kağıt,kitap
+//Student ne yapar? = okur,yazar,ders,sınava girer,dersi dinler,
+//fieldlar:name,last name,class,
+//Student neyi bilir? = öğretmenin ona öğrettiği şeyi
+
+
+import java.util.List;
+
+public class Student {
+
+    String firstName;
+
+    String lastName;
+
+    String Class;
+
+    List<Student> studentList;
+
+
+
+}
